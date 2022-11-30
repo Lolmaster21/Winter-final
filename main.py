88eb7e6ad78b3b93cf29795d3610eb0670115282
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-11-29 22:01:46 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from settings import *
+from level import Level
+
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.clock =pygame.time.Clock()
+        self.level = Level()
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                    
+            dt= self.clock.tick()/1000
+            self.level.run(dt)
+            pygame.display.update()
+        #end of game loop------------------------------------------
+            
+
+#---------- main(starting point of program)-------------------------------------
+if __name__=='__main__':
+    game = Game()
+    game.run()
+
