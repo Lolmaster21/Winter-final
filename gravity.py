4af4e30363d29754406a2bd78e574920166205b3
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-11-29 22:36:34 -0700

    Create gravity.py

---
+++ gravity.py
@@ -0,0 +1,74 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self,pos,group):
+        super().__init__(group)
+        self.image = pygame.Surface((32,64))
+        self.image.fill('blue')
+        self.rect = self.image.get_rect(center = pos)
+
+        self.isOnGround = False
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
+                self.direction.y = -1
+                self.direction.x = -1
+
+        if keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
+                self.direction.y = 1
+                self.direction.x = 1
+
+        if keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
+                self.direction.y = 1
+                self.direction.x = -1
+
+        if keys[pygame.K_SPACE]:
+                self.direction.y = -5
+
+
+        elif keys[pygame.K_UP]:
+                self.direction.y = -1
+                self.status = "up"
+        
+        elif keys[pygame.K_DOWN]:
+                self.direction.y = 1
+                self.status = "down"
+        
+        elif keys[pygame.K_RIGHT]:
+                self.direction.x = 1
+                self.status = "right"
+
+        elif keys[pygame.K_LEFT]:
+                self.direction.x = -1
+                self.status = "left"
+
+        else:
+                self.direction.x = 0
+                self.direction.y = 0
+
+        
+    def gravity():
+        if direction.y > 500 -20:
+            isOnGround = True
+        else:
+            direction.y = False
+
+        if isOnGround == False:
+         direction.y+=.1 #if not on ground, fall downwards
+
+
+    def move(self, dt):
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = self.pos
+
+    def update(self,dt):
+        self.input()
+        self.move(dt)
+    
