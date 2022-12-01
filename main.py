a4a2551278389cc38cd352fcbbd53c8b67bbc9d5
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-12-01 09:58:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import pygame
 from settings import *
 from level import Level
+from Platform import *
 
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
         self.clock =pygame.time.Clock()
         self.level = Level()
         
-
-        
     def run(self):
         while True:
-            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                
                     
             dt= self.clock.tick()/1000
             self.level.run(dt)
@@ -32,4 +28,3 @@ class Game:
 if __name__=='__main__':
     game = Game()
     game.run()
-
