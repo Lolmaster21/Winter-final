67f2d1d84ccb8f0f1f02ef46f1cc6be4e8288318
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-01 21:46:57 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,16 @@
-import pygame
+LEVEL_ONE = [
+'                            ',#1
+'                            ',#2
+' xx    xxx             xx   ',#3
+' xx                         ',#4
+' xxxx        xx          xx ',#5
+' xxxx       xx              ',#6
+' xx    x  xxxx    xx  xx    ',#7
+'       x  xxxx    xx  xxx   ',#8
+'    xxxx  xxxxxx  xx  xxxx  ',#9
+'xxxxxxxx  xxxxxx  xx  xxxx  ']#10
+#1234567890123456789012345679
+
+tile_size = 64
 SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+SCREEN_HIEGHT = len(LEVEL_ONE * tile_size)
