9be5a30b85695ddd4758f2a3589083591ceaba6b
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-02 09:00:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class Game():
         self.clock = py.time.Clock()
         self.level = Level(LEVEL_ONE,self.screen)
         self.start = False
-        self.length = 50
 
         #mouse
         self.mouse = Mouse()
@@ -33,17 +32,17 @@ class Game():
                 self.click = self.mouse.click
 
             #Start
-            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-self.length,400,100,50))
+            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-50,400,100,50))
             if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
                 (self.mouse_pos[1]) > 400 and self.mouse_pos[1] < 450) and self.click == True ):
                 self.start = True
             #Settings
-            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-self.length,500,100,50))
+            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-50,500,100,50))
             if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
                 (self.mouse_pos[1]) > 500 and self.mouse_pos[1] < 550) and self.click == True ):
                 pass
             #Quit
-            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-self.length,600,100,50))
+            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-50,600,100,50))
             if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
                 (self.mouse_pos[1]) > 600 and self.mouse_pos[1] < 650) and self.click == True ):
                 py.quit()
@@ -60,10 +59,12 @@ class Game():
                     py.quit()
                     sys.exit()
 
-            dt = self.clock.tick()/1000 #FPS HANDLER
-            self.level.run(dt)
+            self.screen.fill('black')
+            self.level.run()
             py.display.update()#WORKS LIKE FLIP BUT CAN UPDAT PORTIONS OF THE SCREEN IF WE WANT
-        #END-OF-GAME-LOOP--------------------------------------------------------------------------
+            self.clock.tick(60)
+            #END-OF-GAME-LOOP--------------------------------------------------------------------------
 if __name__ == '__main__':
     g = Game()
     g.Start()#RUNS THE GAME LOOP
+
