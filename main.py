ec7abc55e7a5fe36d0590f3356d60bece9ac5f0e
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-01 21:47:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,69 @@
-import pygame
+import pygame as py
 from settings import *
 from level import Level
-from Platform import *
+from mouse import Mouse
+import sys
 
-
-
-class Game:
+#Class-Game-------------------------------------
+class Game():
     def __init__(self):
-        pygame.init()
-        self.clock =pygame.time.Clock()
-        self.level = Level()
-        
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                    
-            dt= self.clock.tick()/1000
-            self.level.run(dt)
-            pygame.display.update()
-        #end of game loop------------------------------------------
-            
+        py.init()
+        self.screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HIEGHT))
+        self.clock = py.time.Clock()
+        self.level = Level(LEVEL_ONE,self.screen)
+        self.start = False
+        self.length = 50
 
-#---------- main(starting point of program)-------------------------------------
-if __name__=='__main__':
-    game = Game()
-    game.run()
+        #mouse
+        self.mouse = Mouse()
+        self.click = self.mouse.click
+        self.mouse_pos = self.mouse.mouse_pos
+
+    def Start(self):
+        
+        while self.start == False:
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    py.quit()
+                    sys.exit()
+
+                #mouse
+                self.mouse.handler(event)
+                self.mouse_pos = self.mouse.mouse_pos
+                self.click = self.mouse.click
+
+            #Start
+            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-self.length,400,100,50))
+            if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
+                (self.mouse_pos[1]) > 400 and self.mouse_pos[1] < 450) and self.click == True ):
+                self.start = True
+            #Settings
+            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-self.length,500,100,50))
+            if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
+                (self.mouse_pos[1]) > 500 and self.mouse_pos[1] < 550) and self.click == True ):
+                pass
+            #Quit
+            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-self.length,600,100,50))
+            if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
+                (self.mouse_pos[1]) > 600 and self.mouse_pos[1] < 650) and self.click == True ):
+                py.quit()
+                sys.exit()
+            py.display.update()
+        #starts the game using run function
+        while self.start == True:
+            g.run()
+
+    def run(self):
+        while True: #GAME-LOOP---------------------------------------
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    py.quit()
+                    sys.exit()
+
+            dt = self.clock.tick()/1000 #FPS HANDLER
+            self.level.run(dt)
+            py.display.update()#WORKS LIKE FLIP BUT CAN UPDAT PORTIONS OF THE SCREEN IF WE WANT
+        #END-OF-GAME-LOOP--------------------------------------------------------------------------
+if __name__ == '__main__':
+    g = Game()
+    g.Start()#RUNS THE GAME LOOP
