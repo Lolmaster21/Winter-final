c449f126f752393f80e40343ef1f07eef3642eb3
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-02 08:53:35 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,54 +1,49 @@
-import pygame
+import pygame 
 from settings import *
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self,pos,group):
-        super().__init__(group)
+    def __init__(self,pos):
+        super().__init__()
         self.image = pygame.Surface((32,64))
         self.image.fill('blue')
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(topleft = pos)
 
         self.isOnGround = False
-        self.direction = pygame.math.Vector2()
-        self.pos = pygame.math.Vector2(self.rect.center)
-        self.speed = 200
+        self.direction = pygame.math.Vector2(0,0)
+        self.speed = 8
       
     def gravity(self):
-        if self.pos.y > len(LEVEL_ONE * tile_size)-32:
+        if self.rect.y > len(LEVEL_ONE * tile_size)-32:
             self.isOnGround = True
-            self.pos.y = len(LEVEL_ONE * tile_size)-32 #Position
+            self.rect.y = len(LEVEL_ONE * tile_size)-64 #Position
         else: 
             self.isOnGround = False
 
         if self.isOnGround == False: #Gravity function 
             self.direction.y += .2
-
-        self.pos.y +=self.direction.y
+         
+        self.rect.y += self.direction.y 
 
     def input(self):
            keys = pygame.key.get_pressed()
            if keys[pygame.K_SPACE] == True and self.isOnGround == True:
-               self.direction.y = -50 
+               self.direction.y = -1 
+               
                self.isOnGround = False
-
            elif keys[pygame.K_d]:
                 self.direction.x = 1
                 self.direction.y = 0
-
            elif keys[pygame.K_a]:
                 self.direction.x = -1
-                self.direction.y = 0
+                self.direction.y = 0 
 
            else:
                 self.direction.x = 0
                 self.direction.y = 0
 
-    def move(self, dt):
-        self.pos += self.direction * self.speed * dt
-        self.rect.center = self.pos
-
-    def update(self,dt):
+    def update(self):
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y 
         self.input()
-        self.move(dt)
         self.gravity()
