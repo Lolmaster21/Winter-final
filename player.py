53e00f827a68abeb7157c9e013f06e7439e751d8
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-12-07 08:08:37 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame 
+from attack import Attack
 from settings import *
-
+from mouse import Mouse
 class Player(pygame.sprite.Sprite):
 
     def __init__(self,pos):
@@ -8,42 +9,44 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,64))
         self.image.fill('blue')
         self.rect = self.image.get_rect(topleft = pos)
-
         self.isOnGround = False
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 8
-      
+        self.mouse = Mouse()
+        self.click = self.mouse.click
+        self.mouse_pos = self.mouse.mouse_pos
+        self.start = start
+        
     def gravity(self):
-        if self.rect.y > len(LEVEL_ONE * tile_size)-32:
-            self.isOnGround = True
-            self.rect.y = len(LEVEL_ONE * tile_size)-64 #Position
-        else: 
-            self.isOnGround = False
-
-        if self.isOnGround == False: #Gravity function 
-            self.direction.y += .2
+        #Gravity function 
+        self.direction.y += .8
          
         self.rect.y += self.direction.y 
 
     def input(self):
-           keys = pygame.key.get_pressed()
-           if keys[pygame.K_SPACE] == True and self.isOnGround == True:
-               self.direction.y = -1 
-               
-               self.isOnGround = False
-           elif keys[pygame.K_d]:
-                self.direction.x = 1
-                self.direction.y = 0
-           elif keys[pygame.K_a]:
-                self.direction.x = -1
-                self.direction.y = 0 
+        
+        while self.start == True:
+            for event in pygame.event.get():
+                self.mouse.handler(event)
+                self.mouse_pos = self.mouse.mouse_pos
+                self.click = self.mouse.click
+            if self.click == True:
+                print("click")
+                Attack(self.rect.x,self.rect.y,self.direction.x)
+                
 
-           else:
-                self.direction.x = 0
-                self.direction.y = 0
-
-    def update(self):
-        self.rect.x += self.direction.x * self.speed
-        self.rect.y += self.direction.y 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] == True:
+            self.jump()
+            self.isOnGround = False
+        elif keys[pygame.K_d]:
+            self.direction.x = -1
+        elif keys[pygame.K_a]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+                
+    def jump(self):
+        self.direction.y = -16
+    def update(self, amount):
         self.input()
-        self.gravity()
